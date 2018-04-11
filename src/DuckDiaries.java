74fe75898e1cc009594c598c19922a2eb19e9b87
Ville Pietarinen <ville.pietarinen@gmail.com>
2018-04-11 23:21:35 +0300

    Do total overhaul and add writeDiary & getCurrentDate methods

--- src/DuckDiaries.java
+++ src/DuckDiaries.java
@@ -5,6 +5,10 @@
  */
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -17,11 +21,12 @@ public class DuckDiaries {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		String userAction = "";
+		//user input scanner must be declared here because used as an argument in multiple cases
 		final Scanner systemInReader = new Scanner(System.in);
+		//file setup
 		final File diaryFile = new File("diary.txt");
 		
-		//main commands in array
+		//main commands
 		ArrayList<String> mainCmds = new ArrayList<String>();
 		mainCmds.add("read");
 		mainCmds.add("write");
@@ -29,21 +34,83 @@ public class DuckDiaries {
 		
 		printBanner();
 		
+		//main loop
+		String userTask = "";
 		do {
 			try {
-				userAction = queryAction(systemInReader, mainCmds);
+				userTask = queryTask(systemInReader, mainCmds);
+				//read
+				if(userTask.equals(mainCmds.get(0))) {
+					try {
+						readDiary(diaryFile);
+					}
+					catch(FileNotFoundException err) {
+						System.out.println("ERROR: file not found");
+						System.out.println("Creating diary file...");
+						if(diaryFile.createNewFile()) {
+							System.out.println("File created (diary.txt)");
+						} else {
+							System.out.println("Something went wrong while creating the diary file.");
+						}
+					}
+				}
+				//write
+				if(userTask.equals(mainCmds.get(1))) {
+					writeFile(systemInReader, diaryFile);
+				}
 			}
 			catch(InputMismatchException err) {
 				System.out.println("ERROR: " + err.getMessage());
 			}
-		} while(!userAction.equals("quit"));
+		} while(!userTask.equals("quit"));
 		
-		// in the end: close scanners 
+		System.out.println("See you later!");
+		// in the end, close user input scanner
 		systemInReader.close();
 	}
+
 	
-	private static void readFile(File txt) throws FileNotFoundException {
-		Scanner fileReader = new Scanner(txt);
+	/**
+	 * Prints the startup banner
+	 */
+	private static void printBanner() {
+		System.out.println("*************************");
+		System.out.println("***   Duck Diaries   ****");
+		System.out.println("*************************");
+		System.out.println();
+	}
+	
+	
+	/**
+	 * Queries user input which feature to use
+	 * @return String - user input
+	 * @throws Exception 
+	 * @throws InputMismatchException invalid input from user
+	 */
+	private static String queryTask(Scanner scanr, ArrayList<String> cmds) throws InputMismatchException {
+		String readTask = "";
+		String cmdsStringed = String.join(", ", cmds); //stringify commands for desired print format
+		System.out.print("> Select task [" + cmdsStringed + "]: ");
+		
+		if(scanr.hasNextLine()) {
+			readTask = scanr.nextLine();
+			//invalid command inserted by user
+			if(!cmds.contains(readTask)) {
+				throw new InputMismatchException("invalid input");
+			}
+		}
+
+		return readTask;
+	}
+	
+	
+	/**
+	 * Reads given File object and prints it
+	 * @param f File object
+	 * @throws FileNotFoundException file not found
+	 */
+	private static void readDiary(File f) throws FileNotFoundException {
+		Scanner fileReader = new Scanner(f);
 		String line = "";
 		
 		while (fileReader.hasNext()) {
@@ -53,36 +120,41 @@ public class DuckDiaries {
 		fileReader.close();
 	}
 	
-	/**
-	 * Prints the startup banner
-	 */
 	
-	private static void printBanner() {
-		System.out.println("*************************");
-		System.out.println("***   Duck Diaries   ****");
-		System.out.println("*************************");
-		System.out.println();
+	/**
+	 * Gets current date in ISO 8601 format
+	 * @return String - Current date
+	 */
+	private static String getCurrentDate() {
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter isoFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		return today.format(isoFormatter);
 	}
 	
+	
 	/**
-	 * Queries user input which feature to use
-	 * @return user input
-	 * @throws Exception 
-	 * @throws InputMismatchException invalid input from user
+	 * Writes user's inputted lines to given File object
+	 * @param scanr user input scanner
+	 * @param f File object
+	 * @throws IOException
 	 */
-	private static String queryAction(Scanner scanr, ArrayList<String> cmds) throws InputMismatchException {
-		String readAction = "";
-		String actions = String.join(", ", cmds); //stringify commands for desired print format
-		System.out.print("Select action [" + actions + "]: ");
-		
-		if(scanr.hasNextLine()) {
-			readAction = scanr.nextLine();
-			//invalid command inserted by user
-			if(!cmds.contains(readAction)) {
-				throw new InputMismatchException("invalid input");
+	private static void writeFile(Scanner scanr, File f) throws IOException {
+		// FileWriter class uses boolean value for second appending argument
+		FileWriter writer = new FileWriter(f, true);
+		String readLine = "";
+		writer.write(getCurrentDate() + "\n");
+		System.out.println("New entry for date: " + getCurrentDate());
+		do {
+			System.out.print("> Enter line(s) or [end]: ");
+			if(scanr.hasNext()) {
+				readLine = scanr.nextLine();
+				if(!readLine.equals("end")) {
+					writer.write(readLine + "\n");
+				}
 			}
-		}
-
-		return readAction;
+		} while(!readLine.equals("end"));
+		//end of diary entry
+		writer.write("----------\n\n");
+		writer.close();
 	}
 }
